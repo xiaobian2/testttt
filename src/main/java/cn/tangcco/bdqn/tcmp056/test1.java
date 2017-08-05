1f2acc83f0d2eeabc48a00bff06fa49280e46d7f
yuyang001 <yuyang0520@yeah.net>
2017-08-05 15:27:25 +0800

    新增两个java文件

---
+++ src/main/java/cn/tangcco/bdqn/tcmp056/test1.java
@@ -0,0 +1,12 @@
+package cn.tangcco.bdqn.tcmp056;
+
+/**
+ * Created by yung on 2017/8/5.
+ */
+public class test1 {
+
+    public static void main(String[] args) {
+        System.out.println("试了好几遍，终于好了");
+    }
+
+}
---
+++ src/main/java/cn/tangcco/bdqn/tcmp056/test2.java
@@ -0,0 +1,17 @@
+package cn.tangcco.bdqn.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by yung on 2017/8/5.
+ */
+public class test2 {
+
+    @Test
+    public void print(){
+
+        System.out.println("这就尴尬了");
+
+    }
+
+}
